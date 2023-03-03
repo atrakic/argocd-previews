cbd1255fac2ee401cd357036ab56c488cbaca879
Admir Trakic <atrakic@users.noreply.github.com>
2023-03-03 10:25:21 +0100

    Fix actions

--- scripts/create.sh
+++ scripts/create.sh
@@ -5,11 +5,9 @@ APP_ID="${APP_ID:?Error: APP_ID must be set}"
 REPO="${REPO:?Error: REPO must be set}"
 IMAGE_TAG="${IMAGE_TAG:?Error: IMAGE_TAG must be set}"
 CHART_PATH="${CHART_PATH:?Error: CHART_PATH must be set}"
+HOST="${HOST:-test.127.0.0.1.nip.io}"# If variable not set or null, set it to default
 
-HOST="${HOST:-test.127.0.0.1.nip.io}"       # If variable not set or null, set it to default
-export HOST
-
-YAML=$(echo "$APP_ID" | sed -e 's#/#-#g')   # rewrite
+YAML=$(echo "$APP_ID" | sed -e 's#/#-#g')
 cat preview.yaml | \
   kyml tmpl -e HOST -e APP_ID -e IMAGE_TAG -e REPO -e CHART_PATH\
   | tee charts/previews/templates/"$YAML".yml
