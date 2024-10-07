715605a5c38260c750aab33bcb863429fbb7cae0
atrakic <atrakic@users.noreply.github.com>
2024-10-08 00:20:15 +0200

    Update e2e.sh

--- tests/e2e.sh
+++ tests/e2e.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
+
+echo "Waiting for pods to all come up"
+until ! kubectl get po -A | grep ContainerCreating; do
+    echo "Pods still creating"
+    sleep 5
+done
+
 curl -fisk localhost:80 -H "Host: $HOST"
